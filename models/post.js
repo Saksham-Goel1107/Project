4f32dba881575431652b15ec19d7a7a379776dac
Saksham-Goel1107 <sakshamgoel1107@gmail.com>
2025-03-20 14:10:16 +0530

    eigth commit

--- models/post.js
+++ models/post.js
@@ -4,12 +4,6 @@ const postSchema = new mongoose.Schema({
   title: String,
   content: String,
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  comments: [
-    {
-      user: { type: mongoose.Schema.ObjectId, ref: "User" },
-      text: { type: String, required: true }, 
-    }
-  ],
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", default: [] }],
 });
 
